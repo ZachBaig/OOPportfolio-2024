f79af9544388c42b9ba854eb43cd1c47a71f47aa
c3675676 <c3675676@JG103-P09.dmc.leedsbeckett.ac.uk>
2024-01-29 15:53:52 +0000

    CreatedPersonalDetails

---
+++ WeeklyExercises/1GettingStarted/PersonalDetails.java
@@ -0,0 +1,13 @@
+
+public class PersonalDetails {
+
+	public static void main(String[] args) 
+	{
+		System.out.println ("Name: Zach");
+		System.out.println ("Address: 64 Zoo Lane");
+		System.out.println ("Age: 18");
+		System.out.println ("Phone number: 07305594832");
+
+	}
+
+}
