ba80dca0da73b3863724d8bfe8987431e1a0cbc2
c3675676 <c3675676@JG103-P09.dmc.leedsbeckett.ac.uk>
2024-01-29 15:49:18 +0000

    Task2

--- WeeklyExercises/1GettingStarted/Test.java
+++ WeeklyExercises/1GettingStarted/Test.java
@@ -5,11 +5,14 @@ public class Test
    //----------------------------------------------------
    public static void main (String[] args)
    {
-      System.out.println ("An Emergency Broadcast");
+      System.out.bogus ("An Emergency Broadcast");
    }
 }
 
 // Changing "Test" to "test" the error that appears is "NoClassDefFoundError"
 // Changing "Emergency" to "emergency" does not give an error because it is a print statement
 // Removing the first quotation mark in the string gives the error "Syntax Error, insert"
-// Changing 
+// Changing "main" to "man" does not give an error because it is a function name hat the user can change
+// Changing "println" to "bogus" does not give an error because it is meant to simulate real data
+// Removing the semicolon at the end of the print statement no error appears because it marks the end of a statement, meaning there is no end yet so the code inside will stil function
+// Removing the last bracket in the program does not give an error because it is not relying on that brace
