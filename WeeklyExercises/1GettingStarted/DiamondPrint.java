463102399557378dad098f401e9a44a559858166
c3675676 <c3675676@JG103-P09.dmc.leedsbeckett.ac.uk>
2024-01-29 15:58:03 +0000

    created DiamondPrint

---
+++ WeeklyExercises/1GettingStarted/DiamondPrint.java
@@ -0,0 +1,15 @@
+
+public class DiamondPrint {
+
+	public static void main(String[] args) 
+	{
+		System.out.println ("       *\r\n"
+				+ "      ***\r\n"
+				+ "     *****\r\n"
+				+ "    *******\r\n"
+				+ "     *****\r\n"
+				+ "      ***\r\n"
+				+ "       *");
+	}
+
+}
